804e1461a062ada073704961d20b69830f9b4f0c
Michel Bartels <login@michelbartels.com>
2025-02-01 20:53:03 +0000

    working?

--- pyro_vae.py
+++ pyro_vae.py
@@ -8,30 +8,32 @@ from mnist import mnist_bar
 
 latent_dim = 16
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 def linear_init(in_features, out_features, name):
     pyro.sample(name + ".weight", dist.Normal(0., 0.001).expand([in_features, out_features]).to_event(2))
-    pyro.sample(name + ".bias", dist.Normal(0., 0.001).expand([out_features]).to_event(1))
+    pyro.sample(name + ".bias", dist.Normal(0., 0.001).expand([1, out_features]).to_event(2))
 
 def linear_params(x, in_features, out_features, name):
-    weight_mean = pyro.param(name + ".weight.mean", torch.zeros(in_features, out_features))
-    weight_std = pyro.param(name + ".weight.std", torch.ones(in_features, out_features) * 0.001)
+    weight_mean = pyro.param(name + ".weight.mean", torch.zeros(in_features, out_features, device=device))
+    weight_std = pyro.param(name + ".weight.std", torch.ones(in_features, out_features, device=device) * 0.001)
     weight = pyro.sample(name + ".weight", dist.Normal(weight_mean, weight_std).to_event(2))
-    bias_mean = pyro.param(name + ".bias.mean", torch.zeros(out_features))
-    bias_std = pyro.param(name + ".bias.std", torch.ones(out_features))
-    bias = pyro.sample(name + ".bias", dist.Normal(bias_mean, bias_std).to_event(1))
+    bias_mean = pyro.param(name + ".bias.mean", torch.zeros(1, out_features, device=device))
+    bias_std = pyro.param(name + ".bias.std", torch.ones(1, out_features, device=device))
+    bias = pyro.sample(name + ".bias", dist.Normal(bias_mean, bias_std).to_event(2))
     return x @ weight + bias
 
 def linear_params_init(in_features, out_features, name):
-    weight_mean = pyro.param(name + ".weight.mean", torch.zeros(in_features, out_features))
-    weight_std = pyro.param(name + ".weight.std", torch.ones(in_features, out_features) * 0.001)
+    weight_mean = pyro.param(name + ".weight.mean", torch.zeros(in_features, out_features, device=device))
+    weight_std = pyro.param(name + ".weight.std", torch.ones(in_features, out_features, device=device) * 0.001)
     pyro.sample(name + ".weight", dist.Normal(weight_mean, weight_std).to_event(2))
-    bias_mean = pyro.param(name + ".bias.mean", torch.zeros(out_features))
-    bias_std = pyro.param(name + ".bias.std", torch.ones(out_features))
-    pyro.sample(name + ".bias", dist.Normal(bias_mean, bias_std).to_event(1))
+    bias_mean = pyro.param(name + ".bias.mean", torch.zeros(1, out_features, device=device))
+    bias_std = pyro.param(name + ".bias.std", torch.ones(1, out_features, device=device))
+    pyro.sample(name + ".bias", dist.Normal(bias_mean, bias_std).to_event(2))
 
 def linear(x, in_features, out_features, name):
     weight = pyro.sample(name + ".weight", dist.Normal(0., 1.).expand([in_features, out_features]).to_event(2))
-    bias = pyro.sample(name + ".bias", dist.Normal(0., 1.).expand([out_features]).to_event(1))
+    bias = pyro.sample(name + ".bias", dist.Normal(0., 1.).expand([1, out_features]).to_event(2))
     return x @ weight + bias
 
 
@@ -51,25 +53,29 @@ def decoder_params_init():
     linear_init(512, 784, "decoder.output")
 
 def decoder(z):
-    hidden = torch.tanh(linear(z, 784, 512, "decoder.hidden"))
+    hidden = torch.tanh(linear(z, latent_dim, 512, "decoder.hidden"))
     img = torch.sigmoid(linear(hidden, 512, 784, "decoder.output"))
     return img
 
 def model(x):
     with pyro.plate("data", x.shape[0]):
         encoder_init()
-        z_loc = torch.zeros(x.shape[0], latent_dim)
-        z_scale = torch.ones(x.shape[0], latent_dim)
+        z_loc = torch.zeros(x.shape[0], latent_dim, device=device)
+        z_scale = torch.ones(x.shape[0], latent_dim, device=device)
         z = pyro.sample("latent", dist.Normal(z_loc, z_scale).to_event(1))
+        z = z.unsqueeze(1)
         loc_img = decoder(z)
+        loc_img = loc_img.squeeze(1)
         pyro.sample("obs", dist.Normal(loc_img, 1.).to_event(1), obs=x)
 
 def guide(x):
     with pyro.plate("data", x.shape[0]):
         decoder_params_init()
+        x = x.unsqueeze(1)
         mean, log_var = encoder_params(x)
+        mean = mean.squeeze(1)
+        log_var = log_var.squeeze(1)
         var = torch.exp(log_var)
-        print("guide", mean.shape, var.shape)
         pyro.sample("latent", dist.Normal(mean, var).to_event(1))
 
 optimizer = AdamW({"lr": 1.0e-4})
@@ -80,7 +86,7 @@ svi = SVI(model, guide, optimizer, loss=elbo)
 update_loss, mnist = mnist_bar()
 
 for x in mnist():
-    if False:
+    if torch.cuda.is_available():
         x = x.cuda()
     loss = svi.step(x)
     update_loss(loss)
