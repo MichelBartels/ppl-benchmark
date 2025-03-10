36842d4d2dabdf002239d97f5d2d8043c38de586
Michel Bartels <login@michelbartels.com>
2025-03-10 17:50:56 +0000

    add ocaml benchmark

---
+++ ocaml_benchmark/bin/main.ml
@@ -0,0 +1,85 @@
+open Iree_bindings
+open Dsl
+
+module Device =
+  ( val Pjrt_bindings.make
+          "/home/michel/part-ii-project/xla/bazel-bin/xla/pjrt/c/pjrt_c_api_cpu_plugin.so"
+    )
+
+module Runtime = Runtime.Make (Device)
+open Runtime
+
+let benchmark_fn f size =
+  let f = compile [] @@ fun Ir.Var.List.[] -> E (mean [0] @@ f [size]) in
+  fun () ->
+    let y = f [] in
+    ignore @@ DeviceValue.to_host_value y
+
+let uniform = benchmark_fn (uniform ~.0. ~.1.)
+
+let normal = benchmark_fn (norm ~.0. ~.1.)
+
+module Result = struct
+  type t = {times: float list}
+
+  let to_json {times} = `List (List.map (fun x -> `Float x) times)
+end
+
+let benchmark f n_warmup n_bench =
+  for _ = 1 to n_warmup do
+    ignore @@ f
+  done ;
+  let times =
+    List.init n_bench (fun _ ->
+        Core.Time_ns.(
+          let start_time = now () in
+          f () ;
+          let end_time = now () in
+          let elapsed_time = diff end_time start_time in
+          let ns = Span.to_ns elapsed_time in
+          Base.Float.to_float ns /. 1e6 ) )
+  in
+  Result.{times}
+
+module StringMap = Map.Make (String)
+
+module Grid = struct
+  type t = {results: Result.t StringMap.t; label: string}
+
+  let to_json {results; label} =
+    `Assoc
+      [ ("label", `String label)
+      ; ( "results"
+        , `Assoc
+            ( StringMap.bindings results
+            |> List.map (fun (name, result) -> (name, Result.to_json result)) )
+        ) ]
+
+  let save grid filename =
+    let json = to_json grid in
+    Yojson.Basic.to_file filename json
+end
+
+let benchmark_grid f label values n_warmup n_bench =
+  let results =
+    List.fold_left
+      (fun acc value ->
+        print_int value ;
+        let f = f value in
+        let times = benchmark f n_warmup n_bench in
+        StringMap.add (string_of_int value) times acc )
+      StringMap.empty values
+  in
+  Grid.{results; label}
+
+let rec range start stop step =
+  if start >= stop then [] else start :: range (start + step) stop step
+
+let sizes =
+  range (Base.Int.pow 2 24) (Base.Int.pow 2 27 + 1) (Base.Int.pow 2 24)
+
+let () =
+  let grid = benchmark_grid uniform "This implementation" sizes 100 1000 in
+  Grid.save grid "uniform.json" ;
+  let grid = benchmark_grid normal "This implementation" sizes 100 1000 in
+  Grid.save grid "normal.json"
